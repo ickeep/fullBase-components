6f9c90f58c52cb64a469d3d7aca447c2cb7de457
keep <i@ickeep.com>
2018-12-04 15:17:43 +0800

    Link

--- src/unit/link.js
+++ src/unit/link.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 export default class extends Component {
+  static propTypes = {
+    href: PropTypes.string,
+  }
+
   render() {
     const { props } = this
     const { href = '', children = null } = props
