e27747ad3275c11a21a4b3c761a6e9d23804cb07
Keep <i@ickeep.com>
2019-04-08 19:45:58 +0800

    测试

--- example/src/page/index.tsx
+++ example/src/page/index.tsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
-import { Link, Svg } from 'fullbase-components'
+import { Svg, Captcha } from 'fullbase-components'
+import { Link } from 'react-router-dom'
 
 
 /*<Provider Auth={Auth} UI={UI}>*/
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                {/*<Link href="/login">login</Link>*/}
-                <Svg src="404"/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/*<Link href="/login">login</Link>*/}
+        <Link to='login'>登录</Link>
+        <Svg src="404"/>
+        <Captcha value='123' />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { default as Link } from './display/link'
 export { default as Svg } from './display/svg'
+export { default as Captcha } from './form/item/captcha'
 
