6bcd08cbaf0bb275eb4c9cbf8679d5c3296e25ca
Keep <i@ickeep.com>
2019-05-23 14:09:45 +0800

    layout

---
+++ src/layout/unit/footer.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return <div id="b-footer">©2019 admin.kidhappy.cn</div>
+    // return null
+  }
+}
