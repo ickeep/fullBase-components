13ce6059b8c3311218a5fb57ca3739f2f17aab5c
Keep <i@ickeep.com>
2019-07-16 11:48:19 +0800

    表格配置

--- example/src/store/system/_page/tableConf.tsx
+++ example/src/store/system/_page/tableConf.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
-import { Button, Col, Form as FormC, Row } from "antd";
+import { Button, Col, Form as FormC, InputNumber, Row } from "antd";
 import { Curd, Form, Input, Select, Link } from 'fullbase-components'
 import PropsEdit from './propsEdit'
 
@@ -36,7 +36,17 @@ const typeProps = {
 export default class TableConf extends Component<any> {
   add = () => {
     const { value = [], onChange, values, field } = this.props
-    value.push({ field: '', align: '', type: '', dataIndex: '', width: '', fixed: '', props: [], })
+    value.push({
+      field: '',
+      align: '',
+      type: '',
+      dataIndex: '',
+      width: '',
+      fixed: '',
+      props: [],
+      rule: '',
+      expression: ''
+    })
     values[field] = value
     onChange(values)
   }
@@ -78,6 +88,11 @@ export default class TableConf extends Component<any> {
             </FormC.Item>
           </Col>
           <Col span={8}>
+            <FormC.Item label="index">
+              <Input value={item.dataIndex} onChange={(v: any) => this.change(v, index, 'dataIndex')}/>
+            </FormC.Item>
+          </Col>
+          <Col span={8}>
             <FormC.Item label="type">
               <Select value={item.type} data={typeData} onChange={(v) => this.change(v, index, 'type')}/>
             </FormC.Item>
@@ -89,6 +104,34 @@ export default class TableConf extends Component<any> {
                          onChange={(v: any[]) => this.change(v, index, 'props')}/>
             </FormC.Item>
           </Col>}
+          <Col span={8}>
+            <FormC.Item label="align">
+              <Select value={item.align} data={['left', 'right', 'center']}
+                      onChange={(v) => this.change(v, index, 'align')}/>
+            </FormC.Item>
+          </Col>
+          <Col span={8}>
+            <FormC.Item label="width">
+              <InputNumber value={item.width} onChange={(v) => this.change(v, index, 'width')}/>
+            </FormC.Item>
+          </Col>
+          <Col span={8}>
+            <FormC.Item label="fixed">
+              <Select value={item.fixed} data={['left', 'right']} onChange={(v) => this.change(v, index, 'fixed')}/>
+            </FormC.Item>
+          </Col><Col span={8}>
+          <FormC.Item label="rule">
+            <Select value={item.rule} data={['template', 'arithmetic']}
+                    onChange={(v) => this.change(v, index, 'rule')}/>
+          </FormC.Item>
+        </Col><Col span={8}>
+          <FormC.Item label="expression">
+            <Input value={item.expression} onChange={(v: any) => this.change(v, index, 'expression')}/>
+          </FormC.Item>
+        </Col>
+          <Col span={8}>
+            <Button onClick={() => this.cut(index)}>-</Button>
+          </Col>
         </Row>
       </div>
     )}
