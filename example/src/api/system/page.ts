a8208ffe144d55d2d3ee68af65c2a9cf4148258f
Keep <i@ickeep.com>
2019-07-16 16:27:18 +0800

    api 修正

--- example/src/api/system/page.ts
+++ example/src/api/system/page.ts
@@ -3,17 +3,17 @@ import Http from '../http'
 const { httpPost, httpGet } = Http
 
 export function list(data: any) {
-  return httpGet('/page/system/page/list', data)
+  return httpGet('/api/system/page/list', data)
 }
 
 export function detail(data: any) {
-  return httpGet('/page/system/page/detail', data)
+  return httpGet('/api/system/page/detail', data)
 }
 
 export function add(data: any) {
-  return httpPost('/page/system/page/add', data)
+  return httpPost('/api/system/page/add', data)
 }
 
 export function edit(data: any) {
-  return httpPost('/page/system/page/edit', data)
+  return httpPost('/api/system/page/edit', data)
 }
