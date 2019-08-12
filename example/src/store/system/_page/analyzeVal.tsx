8fc267f7e28dc9997c953172a52e70017b517164
Keep <i@ickeep.com>
2019-08-12 17:55:22 +0800

    Analyze props

--- example/src/store/system/_page/analyzeVal.tsx
+++ example/src/store/system/_page/analyzeVal.tsx
@@ -3,7 +3,6 @@ import ejs from 'ejs/ejs.min'
 
 export default function analyze(conf: any) {
   const { data = {}, rule, expression = '' } = conf
-  console.log(data);
   if (rule === 'template' && expression) {
     return ejs.render(expression, data)
   }
